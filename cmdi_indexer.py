a3013cc6b6566a4fa2951276da4ececa60c69d37
kerim1 <kerim.meijer@di.huc.knaw.nl>
2023-10-23 14:02:05 +0200

    Add indexer for Diplomatieke Getuigenissen and add bulk indexer for CMDI records

--- cmdi_indexer.py
+++ cmdi_indexer.py
@@ -3,88 +3,120 @@ import sys
 from indexer import Indexer
 import json
 from lxml import etree
-import glob, os, codecs
+import glob, os
 import unicodedata
+import elementpath
+import getopt
 
-index = "cmdi"
-fields = "./fields.json"
-records = [ "./records" ]
-
-# Count the arguments
-argc = len(sys.argv) - 1
-
-if (argc >= 1):
-    index = sys.argv[1]
-
-if (argc >= 2):
-    fields = sys.argv[2]
-
-if (argc > 2):
-    pos = 3
-    records = []
-    while (argc >= pos):
-        records.append(sys.argv[pos])
-        pos = pos + 1
-
-print ("index[" + index + "]")
-print ("fields[" + fields + "]")
-print ("records[" + str(records) + "]")
-
-f = open(fields)
-index_object = json.load(f)
-
-
-
-
-config = {
-    "url" : "localhost",
-    "port" : "9200",
-    "index" : index
-}
-indexer = Indexer(config)
-
-
+ns = {"cmd": "http://www.clarin.eu/cmd/"}
 
 def grab_value(path, root, ns):
-    content = root.findall(path, ns)
+    content = elementpath.select(root, path, ns)
     if content and content[0].text is not None:
-        return unicodedata.normalize("NFKD", content[0].text).strip()
+        return unicodedata.normalize("NFKC", content[0].text).strip()
     else:
         return ""
 
-def grab_list(name, path, root, ns):
+def grab_list(path, root, ns):
     ret_arr = []
-    content = root.findall(path, ns)
+    content = elementpath.select(root, path, ns)
     for item in content:
-        buffer = {name : unicodedata.normalize("NFKD", item.text).strip()}
+        buffer = unicodedata.normalize("NFKC", item.text).strip()
         if buffer not in ret_arr:
             ret_arr.append(buffer)
     return ret_arr
 
-def make_json(cmdi):
-    retDict = {}
-    retDict["record"] = cmdi
-    file = etree.parse(cmdi)
-    root = file.getroot()
-    ns = {"cmd": "http://www.clarin.eu/cmd/","xml": "http://www.w3.org/XML/1998/namespace"}
-    for field in index_object:
-        if field["fields"][0]["unbounded"] == "no":
-            retDict[field["name"]] = grab_value(field["fields"][0]["path"], root, ns)
-        else:
-            retDict[field["name"]] = grab_list(field["name"] ,field["fields"][0]["path"], root, ns)
-    print(retDict)
-    indexer.add_to_index(retDict)
+def cmdi_records_generator(records):
+    for dir in records:
+        os.chdir(dir)
+        for cmdi in glob.glob("*.cmdi"):
+            yield os.path.abspath(cmdi)
 
-def processDir(dir):
-    os.chdir(dir)
-    for file in glob.glob("*.cmdi"):
-        make_json(file)
+def record_generator(cmdi_records_gen, fields, add_record=None):
+    f = open(fields)
+    index_object = json.load(f)
 
-for dir in records:
-    processDir(dir)
+    for cmdi in cmdi_records_gen:
+        name = os.path.splitext(os.path.basename(cmdi))[0]
+        try:
+            id = int(name)
+        except ValueError:
+            id = name
+
+        retDict = {'id': id}
+        file = etree.parse(cmdi)
+        root = file.getroot()
+
+        for field in index_object:
+            if field["fields"][0]["unbounded"] == "no":
+                retDict[field["name"]] = grab_value(field["fields"][0]["path"], root, ns)
+            else:
+                retDict[field["name"]] = grab_list(field["fields"][0]["path"], root, ns)
+
+        if add_record:
+            retDict['record'] = add_record(root)
+
+        print(retDict)
+        yield retDict
 
 
+if __name__ == '__main__':
+    index = "cmdi"
+    fields = "./fields.json"
+    records = [ "./records" ]
+    host = 'localhost'
+    port = '9200'
+    user = None
+    password = None
+    use_ssl = False
 
+    opts, args = getopt.getopt(sys.argv[1:], 'h:p:u:w:s')
 
+    # Count the arguments
+    argc = len(args)
 
+    if (argc >= 0):
+        index = args[0]
 
+    if (argc >= 1):
+        fields = args[1]
+
+    if (argc > 2):
+        pos = 2
+        records = []
+        while (argc > pos):
+            records.append(args[pos])
+            pos = pos + 1
+
+    for option, value in opts:
+        if option == '-h':
+            host = value
+        elif option == '-p':
+            port = value
+        elif option == '-u':
+            user = value
+        elif option == '-w':
+            password = value
+        elif option == '-s':
+            use_ssl = True
+
+    print ("index[" + index + "]")
+    print ("fields[" + fields + "]")
+    print ("records[" + str(records) + "]")
+    print ("host[" + host + "]")
+    print ("user[" + (user if user else "<no user>") + "]")
+    print ("password[" + (password if password else "<no password>") + "]")
+    print ("use_ssl[" + str(use_ssl) + "]")
+
+    indexer = Indexer({
+        "host" : host,
+        "port" : port,
+        "http_auth": (user, password) if user and password else None,
+        "index" : index,
+        "use_ssl": use_ssl,
+        "verify_certs": False
+    })
+
+    cmdi_gen = cmdi_records_generator(records)
+    record_gen = record_generator(cmdi_gen, fields)
+    indexer.add_to_index_bulk(record_gen)

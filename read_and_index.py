00b683c9cf288d8616d75540f79ef8d761793829
Meindert Kroese <meindertkroese@mac.home>
2025-09-17 13:51:07 +0200

    working service for indexing single files

--- read_and_index.py
+++ read_and_index.py
@@ -8,15 +8,18 @@ import jmespath
 import json
 import locale
 locale.setlocale(locale.LC_ALL, 'nl_NL')
+import os
 from rdflib import Graph
 import sys
 from saxonche import PySaxonProcessor
 import tomllib
-from procrustus_indexer import build_indexer
+from procrustus_indexer import build_indexer,Indexer
+
+ELASTIC_PASSWORD = ''
 
 
-def stderr(text):
-    sys.stderr.write("{}\n".format(text))
+def stderr(text,nl="\n"):
+    sys.stderr.write(f"{text}{nl}")
 
 
 def end_prog(code=0):
@@ -31,7 +34,6 @@ def arguments():
     ap.add_argument('-d', '--directory',
                     help="input directory")
     ap.add_argument('-t', '--tomlfile',
-                    default='ineo.toml',
                     help="toml file")
     ap.add_argument('-f', '--inputfile',
                     help="input file")
@@ -40,41 +42,47 @@ def arguments():
     args = vars(ap.parse_args())
     return args, ap
 
+def read_and_index(toml_file: str, input_dir: str| None=None, input_file: str| None=None, index_name: str | None=None, index_host: str | None=None, force: bool | None=True):
+    stderr(datetime.today().strftime("start: %H:%M:%S"))
+    with open(toml_file, "rb") as f:
+        config = tomllib.load(f)
+
+    extension = config['index']['input']['format']
+    stderr(f'extension: {extension}')
+    if input_dir:
+        input_list = glob.glob(f'{input_dir}/*.{extension}')
+    elif input_file:
+        input_list = [input_file]
+    else:
+        stderr(f"ERR: no input file or dir!")
+        return "ERR: no input file or dir!"
+
+    index = "test"
+    if 'name' in config['index']:
+        index = config['index']['name']
+    if index_name:
+        index = index_name
+    stderr(f"INDEX[{index}]")
+
+    host = os.getenv("ES_URL", "http://localhost:9200/")
+    if 'host' in config['index']:
+        host = config['index']['host']
+    if index_host:
+        host = index_host
+    stderr(f"HOST[{host}]")
+
+#    indexer = Indexer(Elasticsearch(hosts=host,verify_certs=False), config, index)
+    indexer = build_indexer(toml_file, index, Elasticsearch(hosts=host,basic_auth=("elastic", ELASTIC_PASSWORD)))
+
+    indexer.create_mapping(overwrite=force)
+    indexer.import_files(input_list)
+    return "OK"
+
 
 def main():
     stderr(datetime.today().strftime("start: %H:%M:%S"))
     args, ap = arguments()
-    toml_file = args['tomlfile']
-    with open(toml_file, "rb") as f:
-        config = tomllib.load(f)
-
-    for key in config['index']['facet'].keys():
-                    facet = config["index"]["facet"][key]
-                    path_facet = facet["path"]
-
-    input_dir = args['directory']
-    extension = config['index']['input']['format']
-    input_list = glob.glob(f'{input_dir}/*.{extension}')
-    if len(input_list) == 0:
-        input_file = args['inputfile']
-        if input_file is None:
-            end_prog(1)
-        input_list = [args['input_file']]
-
-    index = args['index']    
-    if 'name' in config['index']:
-        index = config['index']['name']
-
-    host="http://localhost:9200/"
-    if 'host' in config['index']:
-        host = config['index']['host']
-
-    #indexer = Indexer(Elasticsearch(hosts=host), config, index)
-    indexer = build_indexer(toml_file, 'index-name', Elasticsearch(hosts=host))
-
-    indexer.create_mapping(overwrite=args['force'])
-    indexer.import_files(input_list)
-
+    read_and_index(toml_file=args['tomlfile'], input_dir=args['directory'], index_name= args['index'], force=args['force'])
     end_prog(0)
 
 
--- service.py
+++ service.py
@@ -2,7 +2,6 @@
 from flask import Flask,render_template,request, jsonify
 import sys
 import tomllib
-sys.path.append("/Users/meindertkroese/git/procrustus-indexer")
 import read_and_index
 import runpy
 import os
@@ -12,22 +11,6 @@ app = Flask(__name__)
 
 def stderr(text):
     sys.stderr.write(f'{text}\n')
-
-def read(toml_file):
-    with open(toml_file, "rb") as f:
-        return tomllib.load(f)
-
-@app.route('/hello/')
-def hello_world():
-    stderr('hello world')
-    return '<p><b>Hello, World</b></p>'
-
-@app.route('/hello/<name>', methods=['GET'])
-def hello(name=None):
-    data = request.form.to_dict()
-    stderr(data)
-    stderr(jsonify(data))
-    return render_template('hello.html', person=name)
 
 @app.route('/index-page')
 def view_form():
@@ -39,16 +22,16 @@ def get():
     config = request.args.get("config")
     directory = request.args.get("dir")
     filename = request.args.get("file")
-    stderr(filename.__class__)
     if config==None or config=="":
-        return {"response": "No config given"}
+        return render_template('result.html', result="No config given")
     if (directory==None or directory=="") and (filename==None or filename==""):
-        return {"response": "No file or dir given"}
-    # Nu mbv config en file/dir read_and_index.py aanroepen
-    args = " ".join(['-t',config,'-d',directory])
-    os.system(f"exec python3 ./read_and_index.py {args}")
-    # evt result in reponse opnemen
-    return {"response": f"This is a GET request\nusing {config}, index contents of {directory} and/or file {filename}"}
+        return render_template('result.html', result="No file or dir given")
+    res = read_and_index.read_and_index(toml_file=config,input_dir=directory,input_file=filename)
+    if res=='OK':
+        result = 'succes!'
+    else:
+        result = f'failed! {res}'
+    return render_template('result.html', result=result)
 
 @app.route('/index', methods=['POST'])
 def post():
@@ -57,9 +40,6 @@ def post():
         data['configfile'] = request.form['configfile']
         data['config'] = request.form['config']
         data['filetext'] = request.form['filetext']
-    stderr('data read')
-#    data = request.get_json()
-    #stderr(data['config'])
     res = ''
     config = ''
     configfile = data['configfile'].strip()
@@ -67,18 +47,17 @@ def post():
         if not configfile.endswith('.toml'):
             configfile = f'{configfile}.toml'
         extension = tomllib.load(f)['index']['input']['format']
-#        config = read(configfile)
     else:
         res += "No configfile given\n"
     if config.strip()=='':
         config = data['config'].strip()
         res += f'{config}\n'
         if config=='':
-            return f"<p>{res}<br/>No config given, can't index</p>"
+            return render_template('result.html', result=f"<p>{res}<br/>No config given, can't index</p>")
         extension = tomllib.loads(config)['index']['input']['format']
     tobeindexed = data['filetext'].strip()
     if tobeindexed=='':
-        return f'{res}No data to be indexed'
+        return render_template('result.html', result='{res}No data to be indexed')
     try:
         directory = os.environ['PROCRUSTUS_TEMP_DIR']
     except:
@@ -90,19 +69,11 @@ def post():
     datafile = f'{directory}/datafile.{extension}'
     with open(datafile,'w') as uitvoer:
         uitvoer.write(tobeindexed)
-    # indien geen configfile: configdata naar /tmp schrijven
-    # tobeindexed naar /tmp schrijven
-
-
-    #stderr(jsonify(data))
-    #res = runpy.run_path(f'read_and_index.py -t {configfile} -d {directory}')
-#    res = runpy.run_path(f'read_and_index.py -t {configfile} -f {datafile}')
-    sys.argv += ['-f',datafile,'-d',directory,'-i','test-index','--force']
     res = read_and_index.read_and_index(toml_file=configfile,input_file=datafile,index_name='test-index',force=True)
-    if res==0:
+    if res=='OK':
         result = 'succes!'
     else:
-        result = 'failed!'
+        result = f'failed! {res}'
     return render_template('result.html', result=result)
 
 if __name__ == '__main__':
